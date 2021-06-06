e9d4f4ff716bf9b1056c28e553386c60b993cf3b
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-06-06 17:18:40 -0300

    Create aula.js

---
+++ aula12/aula.js
@@ -0,0 +1,33 @@
+Some dois valores lidos e imprima o resultado
+
+var valor1 = 10;
+var valor2 = 20;
+alert(valor1 + valor2)
+
+
+
+==================================================
+
+
+var valor1 = 20
+var valor2 = 10
+var operacao = '/'
+var resultado 
+
+switch(operacao){
+    case '+':
+        resultado = valor1 + valor2
+        alert(resultado)
+    break;
+    case '-':
+        resultado = valor1 - valor2
+        alert(resultado) 
+    break;
+    case '*':
+        resultado = valor1 * valor2
+        alert(resultado)
+    break;
+    case '/':
+        resultado = valor1 / valor2
+        alert(resultado)
+    break;
