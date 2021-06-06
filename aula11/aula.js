b940c9fad42baaac6d2850ce0ae22cbd8ec0320a
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-06-06 17:18:12 -0300

    Create aula.js

---
+++ aula11/aula.js
@@ -0,0 +1,33 @@
+.
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atvVini17</title>
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>AtvVini17 - ALERT</h1>
+    </div>
+</body>
+
+</html>
+--------------------------------- FIM HTML -------------------------------------
+------------------------------------------------------------------------------------
+------------------------------- COMEÇO SCRIPT.JS -------------------------------------
+
+let valor = 9
+
+if (valor == '10') {
+    alert('O valor é igual a 10')
+}else if (valor < '10'){
+    alert('O valor é menor que 10!')
+}else {
+    alert(' O valor é maior que 10!')
+}
+
