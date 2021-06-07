29c5068603fd9f93d7f383518ce7ec540f4f9ef1
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-06-07 17:44:27 -0300

    Create traduzindo_calendário.js

---
+++ aula12/traduzindo_calendário.js
@@ -0,0 +1,12 @@
+<script>
+$(function() {
+    $("#calendario").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+        dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+    });
+});
+</script>
