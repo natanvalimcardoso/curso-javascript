b508d5e8a2871a73333de15faf04689e28e9e352
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-06-18 17:55:59 -0300

    Create potenciatds.js

---
+++ aula06/potenciatds.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JavaScript Exercício</title>
+    <script>
+      document.write("Base: 2" + '<br>');
+      document.write("Expoente: 3" + '<br>');
+      document.write("Resultado: " + (2*2*2));
+    </script>
+  </head>
+  <body>
+    <h1>Exercício 13</h1>
+  </body>
+</html>
